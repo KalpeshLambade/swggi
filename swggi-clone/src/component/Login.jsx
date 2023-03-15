8fc5f7d9b5d7128262d9a3226b245137996a4e4a
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-03-15 23:07:50 +0530

    updated

--- swggi-clone/src/component/Login.jsx
+++ swggi-clone/src/component/Login.jsx
@@ -1,6 +1,37 @@
 import "./Component.css";
+import { useState } from "react";
 
 function Login(props){
+    const[formData, setFormData] =useState({email:'', password:'' });
+
+    function checkLog(e){
+        e.preventDefault();
+
+        var dataFromLs = JSON.parse(localStorage.getItem("userDataR"));
+
+        var flag=false;
+        for(var i=0; i<dataFromLs.length; i++){
+            if(dataFromLs[i].email === formData.email && dataFromLs[i].password === formData.password){
+                flag =true;
+            }
+        }
+        if(flag){
+            setFormData({email:'',password:''});
+            alert("Log in sucessful");
+        }
+        else{
+            setFormData({email:'',password:''});
+            alert("Please check email or password");
+        }
+    }
+
+    function featchData(e){
+        var value = e.target.value;
+        var name = e.target.name;
+        // console.log(name,value) ;
+        setFormData({...formData, [name]:value});
+
+    }
     return(
         <div id='login'>
             <div className="login-page">
@@ -15,12 +46,13 @@ function Login(props){
 
                 <div>
                     <form>
-                        <input type='number' placeholder="Phone Number" />
+                        <input type='email' placeholder="Email" onChange={(e) => {featchData(e)}} name="email" required value={formData.email}  />
+                        <input type='password' placeholder="Password" onChange={(e) => {featchData(e)}} name="password" required value={formData.password} />
                     </form>
                 </div>
 
                 <div>
-                    <button>Continue</button>
+                    <button onClick={(e) => {checkLog(e)}}>Continue</button>
                     <p class='tc'>By creating an account, I accept the <strong>Terms & Conditions & Privacy Policy</strong></p>
                 </div>
             </div>
--- swggi-clone/src/component/SignUp.jsx
+++ swggi-clone/src/component/SignUp.jsx
@@ -1,7 +1,44 @@
 import "./Component.css";
+import { useState } from "react";
 
 function SignUp(props){
+    const[userData, setUserData] = useState({name:'', email:'', password: ''});
 
+    function submit(e){
+        e.preventDefault();
+
+        var dataFromLs = JSON.parse(localStorage.getItem("userDataR")) || [];
+
+        var flag =false;
+        for( var i=0; i<dataFromLs.length; i++){
+            if(dataFromLs[i].email === userData.email){
+                flag =true;
+            }
+        }
+        if(flag){
+            setUserData({...userData, ['email']:''});
+            return alert("email already exsited");
+        }
+        else if( userData.password.length <8){
+            setUserData({...userData, ['password']:''});
+            alert("password must be 8 characters");
+        }
+        else{
+            dataFromLs.push(userData);
+            localStorage.setItem("userDataR",JSON.stringify(dataFromLs));
+            setUserData({name:'', email:'', password: ''});
+            alert("Registration done");
+        }
+        
+    }
+
+    function featchData(e){
+        var value =e.target.value;
+        var name = e.target.name;
+
+        setUserData({...userData, [name]:value});
+
+    }
     return(
         <div id='sign-up'>
             <div className="signUp-page">
@@ -16,15 +53,15 @@ function SignUp(props){
 
                 <div>
                     <form>
-                        <input type='number' placeholder="Phone Number" />
-                        <input type='text' placeholder="Name" />
-                        <input type='email' placeholder="Email" />
+                        <input type='text' placeholder="Name" onChange={(e) => {featchData(e)}} name="name" required value={userData.name}/>
+                        <input type='email' placeholder="Email" onChange={(e) => {featchData(e)}} name="email" required value={userData.email}/>
+                        <input type='password' placeholder="Password" onChange={(e) => {featchData(e)}} name="password" required value={userData.password}/>
                     </form>
                 </div>
 
                 <div>
                     <p>Have a referral code?</p>
-                    <button>Continue</button>
+                    <button onClick={(e) => submit(e)}>Continue</button>
                     <p>By creating an account, I accept the <strong>Terms & Conditions & Privacy Policy</strong></p>
                 </div>
             </div>
